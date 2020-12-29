d9936ee215865400a21a98aaa728963d1c357ddf
Sachin Dahal <sachindahal2001@gmail.com>
2020-12-29 12:19:21 +0545

    setup

---
+++ lib/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xffE84C4F);
+const Color kBackgroundColor = Color(0xffF8F5F2);
+
+const Color kPrimaryColorLight = Color(0xffF8F5F2);
+const Color kPrimaryTextColor = Color(0xff3E4462);
+const Color kSecondaryTextColor = Color(0xff7E7E7E);
+
---
+++ lib/theme/text_styles.dart
@@ -0,0 +1,15 @@
+import 'package:ceemtax_service/theme/colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+final headingH1 = const TextStyle(
+    fontSize: 28, fontWeight: FontWeight.w600, color: kPrimaryTextColor);
+
+final headingH2 = const TextStyle(
+    fontSize: 20, fontWeight: FontWeight.w500, color: kPrimaryTextColor);
+
+final kBodyNormalText = const TextStyle(
+    fontSize: 16, fontWeight: FontWeight.w400, color: kSecondaryTextColor);
+
+final kBodySmallText = const TextStyle(
+    fontSize: 16, fontWeight: FontWeight.w400, color: kPrimaryColor);
