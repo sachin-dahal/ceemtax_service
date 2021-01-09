460719cdf02a29e9866cb9d323518ce2bdc38dcc
Sachin Dahal <sachindahal2001@gmail.com>
2021-01-10 05:29:02 +0545

    fixes

--- lib/modules/features/about_us/view/about_us_page.dart
+++ lib/modules/features/about_us/view/about_us_page.dart
@@ -1,3 +1,4 @@
+import 'package:ceemtax_service/modules/data/models/animation/fade_animation.dart';
 import 'package:ceemtax_service/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -11,53 +12,56 @@ class AboutUsPage extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(height: Get.height / 20),
-            Center(
-              child: Container(
-                height: Get.height / 1.6,
-                width: Get.width / 1.1,
-                decoration: BoxDecoration(
-                  color: kPrimaryColorLight2,
-                  borderRadius: BorderRadius.circular(10.0),
-                  border: Border.all(color: kPrimaryColorLight2),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black38,
-                      offset: Offset(1, 1),
-                      blurRadius: 10.0,
-                    ),
-                  ],
-                ),
-                padding: EdgeInsets.all(10.0),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Image(
-                          image: AssetImage("images/logo_1.png"),
-                          height: Get.height / 4.0,
-                        ),
-                        Expanded(
-                          child: Text(
-                            "Ceem Tax Service is owned by Ceem Financial Services. At Ceem Tax Service, our primary focus is to provide an outstanding service to our clients making each and every return count, helping clients with education and customer care.",
-                            textDirection: TextDirection.ltr,
-                            textAlign: TextAlign.end,
-                            style: GoogleFonts.rubik(
-                                fontSize: 14.0, fontWeight: FontWeight.w600),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10.0),
-                    Expanded(
-                      child: Text(
-                        "We focus our energy everyday on communicating, understanding and meeting the very specific and unique needs of our clients. \n\nOur registered tax return preparers are professional from the start. With Ceem Tax Service, you won’t have to worry whether your taxes are being properly handled. You can have peace-of-mind knowing you chose your tax preparer wisely. ",
-                        textDirection: TextDirection.ltr,
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.rubik(
-                            fontSize: 14.0, fontWeight: FontWeight.w600),
+            FadeAnimation(
+              0.5,
+              Center(
+                child: Container(
+                  height: Get.height / 1.6,
+                  width: Get.width / 1.1,
+                  decoration: BoxDecoration(
+                    color: kPrimaryColorLight2,
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all(color: kPrimaryColorLight2),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black38,
+                        offset: Offset(1, 1),
+                        blurRadius: 10.0,
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Image(
+                            image: AssetImage("images/logo_1.png"),
+                            height: Get.height / 4.0,
+                          ),
+                          Expanded(
+                            child: Text(
+                              "A part of Ceem Financial Services. At Ceem Tax Service, our primary focus is to provide an outstanding service to our clients making each and every return count, helping clients with education and customer care.",
+                              textDirection: TextDirection.ltr,
+                              textAlign: TextAlign.end,
+                              style: GoogleFonts.rubik(
+                                  fontSize: 14.0, fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10.0),
+                      Expanded(
+                        child: Text(
+                          "We focus our energy everyday on communicating, understanding and meeting the very specific and unique needs of our clients. \n\nOur registered tax return preparers are professional from the start. With Ceem Tax Service, you won’t have to worry whether your taxes are being properly handled. You can have peace-of-mind knowing you chose your tax preparer wisely. ",
+                          textDirection: TextDirection.ltr,
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.rubik(
+                              fontSize: 14.0, fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
